1b985fb9b65ae23660ff81d08e2e35d0c5ae409b
marcelo046 <marc3lo046@gmail.com>
2018-10-08 12:54:03 -0300

    data base finished

--- base_de_dados.pl
+++ base_de_dados.pl
@@ -1,32 +1,32 @@
 carros('1A', 'Maserati Assetto Corsa', 3217, 369, 280, 1590, 4.51).
 carros('1B', 'Jaguar XKR ', 3996, 363, 250, 1715, 4.76).
 carros('1C', 'Porsche 911 GT2', 3600, 462, 315, 1451, 4.45).
-carros('1D', 'nome', 3217, 369, 280, 1590, float).
-carros('2A', 'nome', 3217, 369, 280, 1590, float).
-carros('2B', 'nome', 3217, 369, 280, 1590, float).
-carros('2C', 'nome', 3217, 369, 280, 1590, float).
-carros('2D', 'nome', 3217, 369, 280, 1590, float).
-carros('3A', 'nome', 3217, 369, 280, 1590, float).
-carros('3B', 'nome', 3217, 369, 280, 1590, float).
-carros('3C', 'nome', 3217, 369, 280, 1590, float).
-carros('3D', 'nome', 3217, 369, 280, 1590, float).
-carros('4A', 'nome', 3217, 369, 280, 1590, float).
-carros('4B', 'nome', 3217, 369, 280, 1590, float).
-carros('4C', 'nome', 3217, 369, 280, 1590, float).
-carros('4D', 'nome', 3217, 369, 280, 1590, float).
-carros('5A', 'nome', 3217, 369, 280, 1590, float).
-carros('5B', 'nome', 3217, 369, 280, 1590, float).
-carros('5C', 'nome', 3217, 369, 280, 1590, float).
-carros('5D', 'nome', 3217, 369, 280, 1590, float).
-carros('6A', 'nome', 3217, 369, 280, 1590, float).
-carros('6B', 'nome', 3217, 369, 280, 1590, float).
-carros('6C', 'nome', 3217, 369, 280, 1590, float).
-carros('6D', 'nome', 3217, 369, 280, 1590, float).
-carros('7A', 'nome', 3217, 369, 280, 1590, float).
-carros('7B', 'nome', 3217, 369, 280, 1590, float).
-carros('7C', 'nome', 3217, 369, 280, 1590, float).
-carros('7D', 'nome', 3217, 369, 280, 1590, float).
-carros('8A', 'nome', 3217, 369, 280, 1590, float).
-carros('8B', 'nome', 3217, 369, 280, 1590, float).
-carros('8C', 'nome', 3217, 369, 280, 1590, float).
-carros('8D', 'nome', 3217, 369, 280, 1590, float).
+carros('1D', 'Aston Martin Vanquish', 5935, 460, 306, 1863, 4.67).
+carros('2A', 'Rolls Royce Park Ward', 5379, 326, 225, 2480, 5.65).
+carros('2B', 'Mercedes S 63 AMG', 6258, 444, 250, 2035, 5.12).
+carros('2C', 'Daimler Super V8', 3996, 363, 250, 1820, 5.15).
+carros('2D', 'Bentley Continental T', 6750, 426, 270, 2450, 5.22).
+carros('3A', 'Lexus RX 300', 2995, 201, 180, 1865, 4.58).
+carros('3B', 'Mercedes ML 55 AMG', 5439, 347, 235, 2300, 4.64).
+carros('3C', 'Ranger Rover', 4553, 218, 196, 2120, 4.71).
+carros('3D', 'BMW X5', 4398, 286, 207, 2170, 4.67).
+carros('4A', 'Bugatti EB 16.4 Veyron', 7993, 1001, 404, 1380, 4.38).
+carros('4B', 'Ferrari 550 Maranello', 5474, 485, 320, 1690, 4.55).
+carros('4C', 'B. Engineering Edonis', 3760, 680, 365, 1300, 4.35).
+carros('4D', 'Lamborghini Diablo', 5992, 550, 330, 1625, 4.47).
+carros('5A', 'VW D1', 6000, 450, 250, 1750, 4.95).
+carros('5B', 'ItalDesign Scghera', 3000, 400, 310, 1450, 4.32).
+carros('5C', 'Cadillac Imaj', 4200, 431, 300, 1850, 5.10).
+carros('5D', 'Chrysler Chronos', 5980, 350, 280, 1950, 5.22).
+carros('6A', 'Binz XL', 4266, 279, 230, 2180, 5.79).
+carros('6B', 'Hooper Rolls Royce Emperor', 6748, 325, 215, 2440, 5.82).
+carros('6C', 'American Custom Lincon', 5409, 380, 200, 2870, 6.40).
+carros('6D', 'Mayback Langversion', 5600, 470, 250, 2450, 6.10).
+carros('7A', 'Pagani Zonda C12 S', 7010, 550, 350, 1250, 4.40).
+carros('7B', 'Morgan Aero 8', 4398, 286, 258, 1000, 4.09).
+carros('7C', 'Mega Monte Carlo', 5991, 395, 300, 1500, 4.45).
+carros('7D', 'Lotec Sirius', 5987, 1200, 400, 1380, 4.12).
+carros('8A', 'Lexus LS 430', 4293, 282, 250, 1995, 5.00).
+carros('8B', 'BMW 745i', 4398, 333, 250, 1945, 5.00).
+carros('8C', 'Cadillac Deville DTS', 4565, 305, 240, 1843, 5.26).
+carros('8D', 'Audi S8', 4172, 360, 250, 1730, 5.03).
