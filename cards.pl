96b266200a9543bce2b4a54bed3de5fe7c9cc513
IgorVeludo <velvet.guimaraes@gmail.com>
2018-10-12 18:40:40 -0300

    fixing build card

--- cards.pl
+++ cards.pl
@@ -1,4 +1,4 @@
-buildCards(Tipo,Nome,Cilindradas,Potencia,Velocidade,Peso,Comprimento,Is_trunfo,carta(Tipo,Nome,Cilindradas,Potencia,Velocidade,Peso,Comprimento,Is_trunfo)).
+buildCards(Tipo,Nome,Cilindradas,Potencia,Velocidade,Peso,Comprimento,Is_trunfo,carros(Tipo,Nome,Cilindradas,Potencia,Velocidade,Peso,Comprimento,Is_trunfo)).
 isSuperTrunfo(carros(_,_,_,_,_,_,_,1,)).
-get_type(carros(Tipo,_,_,_,_,_,_,_),Tipo).
+getType(carros(Tipo,_,_,_,_,_,_,_),Tipo).
 /*isA(Carro, Is).*/
