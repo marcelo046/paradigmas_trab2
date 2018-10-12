44105d6d2c254a0351dc39752a53394cf0544d60
IgorVeludo <velvet.guimaraes@gmail.com>
2018-10-12 19:16:43 -0300

    is A type method

--- cards.pl
+++ cards.pl
@@ -7,5 +7,14 @@ getPotencia(carros(_,_,_,Potencia,_,_,_,_),Potencia).
 getVelocidade(carros(_,_,_,_,Velocidade,_,_,_),Velocidade).
 getPeso(carros(_,_,_,_,_,Peso,_,_),Peso).
 getComprimento(carros(_,_,_,_,_,_,Comprimento,_),Comprimento).
-/*getSuperTrunfo(carros(_,_,_,_,_,_,_,Is_trunfo),Is_trunfo).
-/*isA(Carro, Is).*/
+getSuperTrunfo(carros(_,_,_,_,_,_,_,Is_trunfo),Is_trunfo).
+
+stringEquals(StringA,StringB,Equal) :-
+    string_to_atom(StringA, Atom),
+    string_to_atom(StringB, Atom2),
+    ((Atom == Atom2) -> Equal = 1; Equal = 0).
+
+isA(Carro, Is) :- /*O super trunfo só perde para as cartas do tipo A */
+  getType(Carro, Tipo),
+  sub_string(Tipo, 0, 1, 1, SubString),
+  stringEquals('A', SubString, Is).
