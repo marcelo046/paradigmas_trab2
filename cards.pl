93849a3a568eede3b0a1f57d4e3f11f0916b2a17
IgorVeludo <velvet.guimaraes@gmail.com>
2018-10-12 18:38:54 -0300

    creating cards exemple

--- cards.pl
+++ cards.pl
@@ -1,1 +1,4 @@
+buildCards(Tipo,Nome,Cilindradas,Potencia,Velocidade,Peso,Comprimento,Is_trunfo,carta(Tipo,Nome,Cilindradas,Potencia,Velocidade,Peso,Comprimento,Is_trunfo)).
 isSuperTrunfo(carros(_,_,_,_,_,_,_,1,)).
+get_type(carros(Tipo,_,_,_,_,_,_,_),Tipo).
+/*isA(Carro, Is).*/
