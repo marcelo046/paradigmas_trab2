c9e07e77c1512aca980b7c21c0f6e20b323f03e8
marcelo046 <marc3lo046@gmail.com>
2018-10-13 14:06:57 -0300

    parte de super trunfo removido

---
+++ base.pl
@@ -0,0 +1,27 @@
+aluno(joao,calculo).
+aluno(maria,calculo).
+aluno(joel,programacao).
+aluno(joel,estrutura).
+
+frequenta(joao,puc).
+frequenta(maria,puc).
+frequenta(joel,ufrj).
+
+professor(carlos,calculo).
+professor(ana,estrutura).
+professor(pedro,programacao).
+
+funcionario(pedro,ufrj).
+funcionario(ana,puc).
+funcionario(carlos,puc).
+
+/* aluno X */
+/* prof Y */
+assiste(X, Y) :- 
+
+/*
+a)  Escreva uma regra que permita estabelecer, dado um X
+    (que representa um aluno) e um Y (que representa um
+    professor), se X assiste as aulas ministradas por Y, ou mesmo
+    se X é aluno de Y.
+*/
